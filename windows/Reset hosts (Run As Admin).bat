0bf2450990271636c1fa814ed2d38364df33998f
M <34193156+some-username-here1@users.noreply.github.com>
2019-05-29 01:03:39 -0700

    Update Reset hosts (Run As Admin).bat

--- windows/Reset hosts (Run As Admin).bat
+++ windows/Reset hosts (Run As Admin).bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-input /p q="Are you sure? Any saved customization in your hosts file with be lost... [Y/N]"
-
-IF /I %q% == "yes" ( help hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( IF /I %q% == "Yes" ( help hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( IF /I %q% == "YES" ( help hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( IF /I %q% == "Y" ( help hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( IF /I %q% == "y" ( help hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( echo "Aborting..." ) ) ) ) )
+input /p q="Restore your hosts to what it was, prior to injection? [Y/N]"
+IF /I %q% == "yes" ( type hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( IF /I %q% == "Yes" ( type hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( IF /I %q% == "YES" ( type hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( IF /I %q% == "Y" ( type hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( IF /I %q% == "y" ( type hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( echo "Aborting..."
+  pause ) ) ) ) )
