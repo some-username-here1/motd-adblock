e0d8ed39c600db457e815870e13370f48df0fc21
M <34193156+some-username-here1@users.noreply.github.com>
2019-05-29 00:49:14 -0700

    Update TF2 Adblock (Run As Admin).bat
    Also to allow user to freely edit new backup file.

--- windows/TF2 Adblock (Run As Admin).bat
+++ windows/TF2 Adblock (Run As Admin).bat
@@ -1,6 +1,9 @@
 @ECHO OFF
-echo "Copying Adblock for TF2 into hosts file..."
-cd ..
+cd %~dp0
+echo Creating backup of hosts file prior to injection...
+copy %WinDir%\System32\drivers\etc\hosts hosts
+echo Injecting TF2 Adblock into hosts file...
+cd..
 type tf2-adblock >> %WinDir%\System32\drivers\etc\hosts
-echo "Done!"
+echo Done!
 pause
