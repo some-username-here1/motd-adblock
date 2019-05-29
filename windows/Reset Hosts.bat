d9dc44a46e9d587027b6b65d5e36f675046d9102
M <34193156+some-username-here1@users.noreply.github.com>
2019-05-29 01:12:43 -0700

    Update and rename Reset hosts.bat to Reset Hosts.bat

--- windows/Reset Hosts.bat
+++ windows/Reset Hosts.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-input /p q="Restore your hosts to what it was, prior to injection? Any changes made on top of the injection will be lost. [Y/N]"
+input /p q="Restore your hosts to what it was, prior to injection? Any custom changes made on top of the injection will be lost. [Y/N]"
 IF /I %q% == "yes" ( type hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( IF /I %q% == "Yes" ( type hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( IF /I %q% == "YES" ( type hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( IF /I %q% == "Y" ( type hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( IF /I %q% == "y" ( type hosts > %WinDir%\System32\drivers\etc\hosts ) ELSE ( echo "Aborting..."
   pause ) ) ) ) )
