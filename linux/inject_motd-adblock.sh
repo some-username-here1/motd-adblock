3b9fe04061b2f684c57ef62bfa6896b599607b1f
M <34193156+some-username-here1@users.noreply.github.com>
2019-07-04 13:57:36 -0500

    Update and rename inject_tf2-adblock.sh to inject_motd-adblock.sh

--- linux/inject_motd-adblock.sh
+++ linux/inject_motd-adblock.sh
@@ -2,5 +2,5 @@
 cat /etc/hosts > ./hosts
 sudo chmod u=rwx,g=rx,o=r ./hosts
 cd ..
-sudo cat ./tf2-adblock >> /etc/hosts
+sudo cat ./motd-adblock >> /etc/hosts
 read -rp "Press any key to continue..." key
