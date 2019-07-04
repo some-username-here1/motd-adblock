5349e14a5b6e87e152da5ef4d6ebb2f1d4730f75
M <34193156+some-username-here1@users.noreply.github.com>
2019-07-04 14:01:29 -0500

    Update Inject Code.bat

--- windows/Inject Code.bat
+++ windows/Inject Code.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-# Code from OneDrive Complete uninstaller batch process for Windows 10, then heavily edited.
+# Code from OneDrive Complete uninstaller batch process for Windows 10, then heavily worn down to the basic Administrator requirements.
 NET SESSION >nul 2>&1
 IF %ERRORLEVEL% == 0 ( echo Administrator Privileges Detected! ) ELSE ( echo This script must be run as administrator to work properly.
   echo Exiting...
@@ -12,6 +12,6 @@ echo Creating backup of hosts file prior to injection...
 copy %WinDir%\System32\drivers\etc\hosts hosts
 echo Injecting TF2 Adblock into hosts file...
 cd..
-type tf2-adblock >> %WinDir%\System32\drivers\etc\hosts
+type motd-adblock >> %WinDir%\System32\drivers\etc\hosts
 echo Done!
 pause
